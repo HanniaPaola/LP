1e9075871761d9fa914379fe05a3c77d2bffba79
HanniaPaola <233273@ids.upchiapas.edu.mx>
2024-09-25 14:45:05 -0600

    Subir cambios

--- src/app/components/card-libros/card-libros.component.ts
+++ src/app/components/card-libros/card-libros.component.ts
@@ -13,27 +13,6 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class CardLibrosComponent {
 
-/* libros: Libro[] = [
-    { titulo: 'Libro 1', descripcion: 'Descripción del libro 1', imagen: 'ruta/a/imagen1.jpg' },
-    { titulo: 'Libro 2', descripcion: 'Descripción del libro 2', imagen: 'ruta/a/imagen2.jpg' },
-    { titulo: 'Libro 3', descripcion: 'Descripción del libro 3', imagen: 'ruta/a/imagen3.jpg' },
-  ];
-
-  // Simular más libros para mostrar al hacer clic
-  masLibros: Libro[] = [
-    { titulo: 'Libro 4', descripcion: 'Descripción del libro 4', imagen: 'ruta/a/imagen4.jpg' },
-    { titulo: 'Libro 5', descripcion: 'Descripción del libro 5', imagen: 'ruta/a/imagen5.jpg' },
-    { titulo: 'Libro 6', descripcion: 'Descripción del libro 6', imagen: 'ruta/a/imagen6.jpg' },
-  ];
-
-  mostrarMas: boolean = false;
-
-  mostrarMasCards() {
-    this.mostrarMas = !this.mostrarMas;
-    if (this.mostrarMas) {
-      this.libros.push(...this.masLibros);
-    }
-  }*/
 }
 
 
